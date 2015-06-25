48818671f7d2e6aedf01782648e796b83c7b3caf
bowen <giftbowen@live.cn>
2015-06-25 20:36:43 +0800

    Use url helper to get action urls instead of hard code them

--- HlxPortal/WebSite/App_Start/WebApiConfig.cs
+++ HlxPortal/WebSite/App_Start/WebApiConfig.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
 
 namespace LeSan.HlxPortal.WebSite
 {
@@ -13,6 +19,42 @@ namespace LeSan.HlxPortal.WebSite
         /// <param name="config">WebApi Config</param>
         public static void Register(HttpConfiguration config)
         {
+            IAssembliesResolver assembliesResolver = config.Services.GetAssembliesResolver();
+
+            ICollection<Assembly> assemblies = assembliesResolver.GetAssemblies();
+
+            StringBuilder errorsBuilder = new StringBuilder();
+
+            foreach (Assembly assembly in assemblies)
+            {
+                Type[] exportedTypes = null;
+                if (assembly == null || assembly.IsDynamic)
+                {
+                    // can't call GetExportedTypes on a dynamic assembly
+                    continue;
+                }
+
+                try
+                {
+                    exportedTypes = assembly.GetExportedTypes();
+                    errorsBuilder.AppendLine("ok webapi register");
+                }
+                catch (ReflectionTypeLoadException ex)
+                {
+                    exportedTypes = ex.Types;
+                }
+                catch (Exception ex)
+                {
+                    errorsBuilder.AppendLine(ex.ToString());
+                }
+            }
+
+            if (errorsBuilder.Length > 0)
+            {
+                //Log errors into Event Log
+                Trace.TraceError(errorsBuilder.ToString());
+            }
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
